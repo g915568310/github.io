cf1743dfcf8e274260e04423198d4b50a99fa8bb
g915568310 <915568310@qq.com>
2022-01-11 17:17:30 +0800

    Create wz.js

---
+++ wz.js/wz.js
@@ -0,0 +1,11 @@
+var wz = new Array(9);//备注：定义0-9共10个网址
+wz[1]="http://api.bemfa.com/api/device/v1/data/1/push/get/?uid=f06cc44ba970497882ebeaabfc782d35&topic=led002&msg=on"
+wz[2]="http://api.bemfa.com/api/device/v1/data/1/push/get/?uid=f06cc44ba970497882ebeaabfc782d35&topic=led002&msg=off"
+wz[3]="写网址3"
+wz[4]="写网址4"
+wz[5]="写网址3"
+wz[6]="写网址3"
+wz[7]="写网址3"
+wz[8]="写网址3"
+//.....依次类推写到wz[9]
+wz[9]="写网址10"
